1f0e383052fcfc1c62520d8b097c2349d071790c
Mawenjing <2805936956@qq.com>
2022-11-28 09:33:30 +0800

    add py

---
+++ 通用工具下载page/1.py
@@ -0,0 +1,19 @@
+# -- coding: utf-8 --
+"""
+@Project : pythonProject_wensi
+@File : 1.py
+@Author : wenjing
+@Date : 2022/11/25 15:20
+"""
+import html
+from urllib.parse import quote
+a = 'https://www.katana-land.de/en/self-defence?page=3'
+print(quote(a))
+print(quote(a.split('://')[1].replace('/','')))
+# www.katana-land.deenself-defence%3Fpage%3D3
+print(html.escape('https://www.katana-land.de/en/self-defence?page=3'.split('://')[1].replace('/','_')))
+
+# sets = ['/', '\\', ':', '*', '?', '"', '<', '>', '|']
+# for char in name:
+#     if char in sets:
+#         name = name.replace(char, '')
